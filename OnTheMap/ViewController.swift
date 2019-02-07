0719080068338e0a3d155032480ec26683e45b0d
Mario Cezzare <mcezzare@gmail.com>
2019-02-06 23:38:28 -0200

    Initial Commit

---
+++ OnTheMap/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  OnTheMap
+//
+//  Created by Mario Cezzare on 06/02/19.
+//  Copyright © 2019 Mario Cezzare. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
