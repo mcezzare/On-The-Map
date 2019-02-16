e4a69c2308378749783bafecf9a9852493499e97
Mario Cezzare <mcezzare@gmail.com>
2019-02-16 19:27:41 -0200

    add missing confirmation post Pin view controller

---
+++ OnTheMap/ViewControllers/PinViewController.swift
@@ -0,0 +1,18 @@
+//
+//  PinViewController.swift
+//  OnTheMap
+//
+//  Created by Mario Cezzare on 16/02/19.
+//  Copyright © 2019 Mario Cezzare. All rights reserved.
+//
+
+import UIKit
+
+class PinViewController : UITabBarController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+}
+
