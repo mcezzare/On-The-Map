e96bf2b513bdb13f46cb41eb11b35488aef08367
Mario Cezzare <mcezzare@gmail.com>
2019-02-16 18:45:51 -0200

    - Create a second storyBoard for the Map Navigation Flow - Add UIViewControllers for new Map StoryBoard Itens - Create a segue from LoginController to reference Map

---
+++ OnTheMap/ViewControllers/ListViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ListViewController.swift
+//  OnTheMap
+//
+//  Created by Mario Cezzare on 16/02/19.
+//  Copyright © 2019 Mario Cezzare. All rights reserved.
+//
+
+import UIKit
+
+class ListViewController : UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+}
+
---
+++ OnTheMap/ViewControllers/PostViewController.swift
@@ -0,0 +1,18 @@
+//
+//  PostViewController.swift
+//  OnTheMap
+//
+//  Created by Mario Cezzare on 16/02/19.
+//  Copyright © 2019 Mario Cezzare. All rights reserved.
+//
+
+import UIKit
+
+class PostViewController : UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+}
+
