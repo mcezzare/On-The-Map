de056c3ba8c36cddaa8ce2acecd0c0c443a7c61c
Mario Cezzare <mcezzare@gmail.com>
2019-02-27 01:02:22 -0300

    Data Loading and click to browser working

--- OnTheMap/ViewControllers/TableViewController.swift
+++ OnTheMap/ViewControllers/TableViewController.swift
@@ -9,30 +9,20 @@
 import Foundation
 import UIKit
 
-class TableViewController : UIViewController, LocationSelectionDelegate  {
-    // Required by protocol , but not working. How to solve this ? 
-    func didSelectLocation(info: StudentInformation) {
-        print("DEBUG: Calling safari")
-        openWithSafari(info.mediaURL)
-    }
-    
+class TableViewController : UIViewController, UITableViewDataSource, UITableViewDelegate {
     
     // MARK: OUTLETS
     @IBOutlet weak var tableView: UITableView!
-    @IBOutlet weak var dataProvider: DataTableProvider!
     @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
-    
     
     // MARK: Lyfe cicle
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.tableView.delegate = self
+        self.tableView.dataSource = self
         
         NotificationCenter.default.addObserver(self, selector: #selector(reloadStarted), name: .reloadStarted, object: nil)
         NotificationCenter.default.addObserver(self, selector: #selector(reloadCompleted), name: .reloadCompleted, object: nil)
-        
-        dataProvider.delegate? = self
-        tableView.dataSource = dataProvider
-        tableView.delegate = dataProvider
         
         reloadCompleted()
     }
@@ -56,6 +46,44 @@ class TableViewController : UIViewController, LocationSelectionDelegate  {
         }
     }
     
+    // MARK:- Delegates
+    
+    // MARK : Access the debug on delegate property
+    let appDelegate = UIApplication.shared.delegate as! AppDelegate
+    
+    // MARK: - UITableViewDataSource
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        let totalStudents = StudentsLocation.shared.studentsInformation.count
+        if appDelegate.DEBUG {
+            print("DEBUG FROM DATA PROVIDER \(totalStudents)")
+        }
+        
+        return totalStudents
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: LocationCell.identifier, for: indexPath) as! LocationCell
+        cell.configWith(StudentsLocation.shared.studentsInformation[indexPath.row])
+        return cell
+    }
+    
+    // MARK: - UITableViewDelegate
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 50
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let studentInfo = StudentsLocation.shared.studentsInformation[indexPath.row]
+        if appDelegate.DEBUG {
+            print("DEBUG: Selecionando cell \(studentInfo)")
+            print("DEBUG URL: \(studentInfo.mediaURL)")
+        }
+        
+        openWithSafari(studentInfo.mediaURL) // this is not working
+        //if when back from browser the item should be not selected anymore
+        //tableView.deselectRow(at: indexPath, animated: true)
+    }
     
 }
 
